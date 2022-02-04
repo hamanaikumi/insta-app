f0fd9f0e32e70abfdf330487a73cd3acf1f9e6da
riho-trm <riho.n.0304+1@gmail.com>
2022-02-04 16:07:27 +0900

    createdの処理、画面アイコン、フォロー情報、自己紹介のtemplate作成

---
+++ pages/Mypage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="top-wrapper">
+    <div class="user-information">
+      <div class="icon">
+        <img v-bind:src="userInformation.icon" />
+      </div>
+      <div class="follow-information">
+        <div class="posts-number">
+          <p>{{ myPost.length }}</p>
+          <p>投稿数</p>
+        </div>
+        <div class="folower-number">
+          <p>{{ userInformation.follower.length }}</p>
+          <p>フォロワー</p>
+        </div>
+        <div class="folow-number">
+          <p>{{ userInformation.follow.length }}</p>
+          <p>フォロー</p>
+        </div>
+      </div>
+      <div class="bio-contents">
+        <p>{{ userInformation.userName }}</p>
+        <p>{{ userInformation.bio }}</p>
+      </div>
+    </div>
+    <div class="contents"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userInformation: {},
+      myPost: [],
+    }
+  },
+  created() {
+    const users = this.$store.getters['sample/getUserInformation']
+    this.userInformation = users[1]
+    const posts = this.$store.getters['sample/getPostInformation']
+    this.myPost = posts.filter(
+      (post) => post.userId === this.userInformation.userId
+    )
+  },
+}
+</script>
+
+<style></style>
