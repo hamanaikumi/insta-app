e914caa932c7b7372246911be26209db5b724a9f
riho-trm <riho.n.0304+1@gmail.com>
2022-02-08 10:28:17 +0900

    マイページユーザー情報部分の外観を実装

--- pages/Mypage.vue
+++ pages/Mypage.vue
@@ -1,29 +1,47 @@
 <template>
-  <div class="top-wrapper">
+  <div class="top-wrapper mt-12 box-border p-5">
     <div class="user-information">
-      <div class="icon">
-        <img v-bind:src="userInformation.icon" />
+      <div class="icon-follow flex flex-row items-center justify-between">
+        <div class="icon">
+          <img
+            v-bind:src="userInformation.icon"
+            class="w-20 h-20 rounded-full"
+          />
+        </div>
+        <div class="follow-information w-2/4 flex flex-row justify-between">
+          <div class="posts-number text-center p-1">
+            <span class="font-medium">{{ myPost.length }}</span>
+            <br />
+            <span class="text-xs">投稿数</span>
+          </div>
+          <div class="folower-number text-center p-1">
+            <span class="font-medium">{{
+              userInformation.follower.length
+            }}</span>
+            <br />
+            <span class="text-xs">フォロワー</span>
+          </div>
+          <div class="folow-number text-center p-1">
+            <span class="font-medium">{{ userInformation.follow.length }}</span>
+            <br />
+            <span class="text-xs">フォロー</span>
+          </div>
+        </div>
       </div>
-      <div class="follow-information">
-        <div class="posts-number">
-          <p>{{ myPost.length }}</p>
-          <p>投稿数</p>
-        </div>
-        <div class="folower-number">
-          <p>{{ userInformation.follower.length }}</p>
-          <p>フォロワー</p>
-        </div>
-        <div class="folow-number">
-          <p>{{ userInformation.follow.length }}</p>
-          <p>フォロー</p>
-        </div>
-      </div>
-      <div class="bio-contents">
-        <p>{{ userInformation.userName }}</p>
-        <p>{{ userInformation.bio }}</p>
+      <div class="bio-contents py-2.5">
+        <span class="font-medium">{{ userInformation.userName }}</span>
+        <br />
+        <span class="text-sm">{{ userInformation.bio }}</span>
       </div>
     </div>
-    <div class="contents"></div>
+    <div class="contents-icon flex h-10 items-center">
+      <div class="w-1/2 text-center">
+        <i class="fas fa-border-all"></i>
+      </div>
+      <div class="w-1/2 text-center">
+        <i class="fas fa-map-marker-alt"></i>
+      </div>
+    </div>
   </div>
 </template>
 
