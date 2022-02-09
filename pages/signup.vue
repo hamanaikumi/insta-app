0b54d72f0dbb4c4ebafc6e37b672d5e5fc388f2c
jurasora1010 <ikumi.hamana@rakus-partners.co.jp>
2022-02-09 09:47:15 +0900

    デザイン修正

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,35 +1,34 @@
 <template>
-  <div class="grid content-center px-4 py-8">
+  <div class="content-center px-4 py-8">
     <div class="flex justify-center mt-16">
       <!-- logo -->
       <img src="https://svgsilh.com/svg/1594387.svg" alt="" width="200" />
     </div>
-    <div class="mt-8">
-      <div class="bg-light-gray pb-2 px-4 rounded-lg">
+    <div class="mt-8 text-xl">
+      <div class="bg-gray-50 py-4 px-4 rounded-lg">
         <div class="border-b border-input-value-color">
-          <label class="text-xs text-input-value-color">Create user name</label>
+          <label class="text-input-value-color">Create user name</label>
           <br />
           <input
-            class="appearance-none bg-transparent border-none focus:outline-none py-2 px-3"
+            class="appearance-none bg-gray-50 border-none focus:outline-none py-2 px-3"
             type="text"
           />
         </div>
       </div>
-      <div class="bg-light-gray pb-2 px-4 rounded-lg mt-8">
+      <div class="bg-gray-50 py-4 px-4 rounded-lg mt-8">
         <div class="border-b border-input-value-color">
-          <label class="text-xs text-input-value-color">Create a password</label
-          ><br />
+          <label class="text-input-value-color">Create a password</label><br />
           <input
-            class="appearance-none bg-transparent border-none focus:outline-none py-2 px-3"
-            type="text"
+            class="appearance-none bg-gray-50 border-none focus:outline-none py-2 px-3"
+            type="password"
           />
         </div>
       </div>
     </div>
-    <div class="grid-cols-8">
+    <div class="mt-12">
       <!-- button -->
       <button
-        class="text-white bg-accent-color focus:ring-4 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center mt-8"
+        class="text-white bg-accent-color focus:ring-4 font-medium rounded-lg text-lg w-full px-5 py-2.5 text-center"
       >
         Create New Account
       </button>
