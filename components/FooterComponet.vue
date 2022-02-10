c0cbbd1e991e39540d88e0d5cffbed10fa249202
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2022-02-10 16:24:16 +0900

    各種ルーターの設定、パスによってアイコンの表示を切り替える機能追加

--- components/FooterComponet.vue
+++ components/FooterComponet.vue
@@ -1,11 +1,14 @@
 <template>
-  <main class="main pt-7 pb-7 text-center shadow-sm flex w-full">
-    <nuxt-link to="/Setting" class="text-center w-[33.33%]">
+  <main class="main pt-7 pb-7 text-center flex w-full bg-white">
+    <!-- 検索アイコン -->
+    <nuxt-link to="/Search" class="w-[33.33%]">
       <i class="fas fa-search fa-lg"></i>
     </nuxt-link>
+    <!-- homeアイコン -->
     <nuxt-link to="/Home" class="w-[33.33%]">
       <i class="fas fa-home fa-lg"></i>
     </nuxt-link>
+    <!-- マイページアイコン -->
     <nuxt-link to="/Mypage" class="w-[33.33%]">
       <i class="fas fa-user-circle fa-lg"></i>
     </nuxt-link>
--- components/HeaderComponet.vue
+++ components/HeaderComponet.vue
@@ -1,16 +1,42 @@
 <template>
-  <main class="main p-4 shadow-sm">
+  <main class="main p-4 bg-white">
     <div class="w-full flex">
-      <p class="inline-block">instagram</p>
+      <p v-if="$route.path !== '/Login' && $route.path !== '/Signup'">
+        instagram
+      </p>
       <div class="w-full text-right">
-        <nuxt-link to="/addPost">
-          <i
-            v-if="$route.path === '/addPost' || $route.path === '/'"
-            class="far fa-plus-square fa-lg fa-fw mr-4"
-          ></i>
+        <!-- 投稿アイコン -->
+        <nuxt-link
+          v-if="
+            $route.path === '/addPost' ||
+            $route.path === '/Mypage' ||
+            $route.path === '/Userpage' ||
+            $route.path === '/Home' ||
+            $route.path === '/Search'
+          "
+          to="/addPost"
+          class="mr-4"
+        >
+          <i class="far fa-plus-square fa-lg fa-fw"></i>
         </nuxt-link>
-        <i class="far fa-heart fa-lg fa-fw mr-4"></i>
-        <nuxt-link to="/Setting">
+        <!-- 通知アイコン（仮） -->
+        <nuxt-link
+          v-if="$route.path === '/Home' || $route.path === '/Search'"
+          to="/"
+          class="mr-4"
+        >
+          <i class="far fa-heart fa-lg fa-fw"></i>
+        </nuxt-link>
+        <!-- 設定アイコン -->
+        <nuxt-link
+          v-if="
+            $route.path === '/addPost' ||
+            $route.path === '/Mypage' ||
+            $route.path === '/Userpage'
+          "
+          to="/Setting"
+          class="mr-4"
+        >
           <i class="fas fa-cog fa-lg fa-fw"></i>
         </nuxt-link>
       </div>
