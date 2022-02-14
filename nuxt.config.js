dfb74e6dceac47de515d9c43805eddd56b038555
riho-trm <riho.n.0304+1@gmail.com>
2022-02-14 15:19:45 +0900

    plugin設定をssrからmodeに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/amCharts.js', ssr: false }],
+  plugins: [{ src: '~/plugins/amCharts.js', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
