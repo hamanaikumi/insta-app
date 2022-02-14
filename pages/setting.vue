08d5f297de700e16553dbc463b49da60cc89fdb9
jurasora1010 <ikumi.hamana@rakus-partners.co.jp>
2022-02-14 09:27:56 +0900

    設定画面を作成

---
+++ pages/setting.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto px-4 py-8"></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {}
+  },
+  methods: {},
+})
+</script>
+<style></style>
