eb67d73184d5091ef3150862e3430b4fae334efb
riho-trm <riho.n.0304+1@gmail.com>
2022-02-15 22:01:44 +0900

    フォロー削除ボタンコンポーネントを作成

---
+++ components/DeleteFollow.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button type="button" @click="onClickDelete()">{{ button }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      button: '削除',
+    }
+  },
+  methods: {
+    onClickDelete() {
+      if (this.button === '削除') {
+        this.$emit('delete')
+        this.button = 'フォロー'
+      } else if (this.button === 'フォロー') {
+        this.$emit('follow')
+        this.button = '削除'
+      }
+    },
+  },
+})
+</script>
+
+<style></style>
