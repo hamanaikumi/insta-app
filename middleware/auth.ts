9044c293908754c9f2ef852f0fd95de714b8f54f
jurasora1010 <ikumi.hamana@rakus-partners.co.jp>
2022-02-25 15:41:41 +0900

    デバッグ用コード削除。jwtの保存期間変更

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,8 +2,6 @@ import { Middleware, Context } from '@nuxt/types'
 import Auth from '@/plugins/auth'
 
 const auth: Middleware = (context: Context) => {
-  console.log(Auth.authenticated(context.$cookies))
-
   if (!Auth.authenticated(context.$cookies)) {
     // 未認証ならログイン画面にリダイレクト
     return context.redirect('/')
