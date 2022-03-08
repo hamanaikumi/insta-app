a69049bcce7b75cf783981a84c849c73825413eb
Mei-K <mei.kikuchi@rakus-partners.co.jp>
2022-03-08 14:41:14 +0900

    仮(戻すかも)

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,6 +37,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
+  target: static,
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
