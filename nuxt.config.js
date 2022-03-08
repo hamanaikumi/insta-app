922ee35454960986af61c09784c32c2c5f82471d
Mei-K <mei.kikuchi@rakus-partners.co.jp>
2022-03-08 14:46:05 +0900

    Netlifyデプロイ試し用(できなかったら戻します)

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,9 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
+
   target: static,
+  ssr: true,
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
